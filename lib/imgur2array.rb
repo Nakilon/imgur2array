e692a42bb6dbfb0f302734c16ef1ea6aff83ace0
Victor Maslov <nakilon@gmail.com>
2017-11-10 17:30:46 +0300

    tests passed

--- lib/imgur2array.rb
+++ lib/imgur2array.rb
@@ -19,10 +19,12 @@ module Imgur
     fail "env var missing -- IMGUR_CLIENT_ID" unless ENV["IMGUR_CLIENT_ID"]
 
     case link
-    when /\Ahttps?:\/\/((m|i|www)\.)?imgur\.com\/(a|gallery|r\/[A-Za-z0-9][A-Za-z0-9_]{2,20})\/[a-zA-Z0-9]{5,7}(#[a-zA-Z0-9]{7})?\z/
-      fail link.inspect unless /\/(?<type>a|gallery|r\/[A-Za-z0-9][A-Za-z0-9_]{2,20})\/(?<id>[a-zA-Z0-9]{5,7})/ =~ link
+    when /\Ahttps?:\/\/((m|i|www)\.)?imgur\.com\/(a|gallery)\/[a-zA-Z0-9]{5}(#[a-zA-Z0-9]{7})?\z/,
+         /\Ahttps?:\/\/imgur\.com\/gallery\/[a-zA-Z0-9]{5}\/new\z/
+      fail link.inspect unless /\/(?<type>a|gallery)\/(?<id>[a-zA-Z0-9]{5})/ =~ link
       json = NetHTTPUtils.request_data "https://api.imgur.com/3/#{
-        type == "gallery" ? "gallery" : "album"
+        # type == "gallery" ? "gallery" : "album"
+        "album"
       }/#{id}/0.json",
         header: { Authorization: "Client-ID #{ENV["IMGUR_CLIENT_ID"]}" }
       data_imgur = JSON.parse(json)["data"]
@@ -39,9 +41,9 @@ module Imgur
         fail data_imgur.inspect
       end
     when /\/\/i\./,
-         /\Ahttps?:\/\/((m|www)\.)?imgur\.com\/[a-zA-Z0-9]{7}(\?r|\.jpg|\.gifv)?\z/
+         /\Ahttps?:\/\/((m|www)\.)?imgur\.com\/(gallery\/|r\/[A-Za-z0-9][A-Za-z0-9_]{2,20}\/)?[a-zA-Z0-9]{7}(\?r|\.jpg|\.gifv)?\z/
       json = NetHTTPUtils.request_data "https://api.imgur.com/3/image/#{
-        link[/[a-zA-Z0-9]{7}/]
+        link[/(?<=\/)[a-zA-Z0-9]{7}(?=[^\/]*\z)/]
       }/0.json",
         header: { Authorization: "Client-ID #{ENV["IMGUR_CLIENT_ID"]}" }
       [ JSON.load(json)["data"] ]
@@ -74,26 +76,24 @@ if $0 == __FILE__
   end
 
   [
-    # ["http://i.imgur.com/7xcxxkR.gifv", "http://i.imgur.com/7xcxxkRh.gif"], # gif
-    # ["http://imgur.com/HQHBBBD", "https://i.imgur.com/HQHBBBD.jpg"], # http
-    # ["https://imgur.com/BGDh6eu", "https://i.imgur.com/BGDh6eu.jpg"], # https
-    # ["https://imgur.com/a/qNCOo", "https://i.imgur.com/vwqfi3s.jpg", "https://i.imgur.com/CnSMWvo.jpg", 6], # https album
-    # ["http://imgur.com/a/0MiUo", "https://i.imgur.com/kJ1jrjO.jpg", "https://i.imgur.com/TMQJARX.jpg", 49], # album, zoomable
-    # ["http://imgur.com/a/WswMg", "https://i.imgur.com/Bt3RWV7.png", "https://i.imgur.com/sRc2lqN.png", 20], # album image, not zoomable
-    # ["http://imgur.com/a/AdJUK", "https://i.imgur.com/Yunpxnx.jpg", "https://i.imgur.com/2epn2nT.jpg", 3], # needs https because of authorship # WAT?
-    
-    # ["http://imgur.com/gallery/nKAwE/new", "http://i.imgur.com/VQhR8hB.jpg"],
-    # ["http://imgur.com/gallery/vR4Am", "http://i.imgur.com/ydDUl4z.jpg"],
-    # ["http://imgur.com/gallery/qP2RQtL", "http://i.imgur.com/qP2RQtL.png"], # single image gallery?
-    # ["http://m.imgur.com/rarOohr", "http://m.imgur.com/rarOohr.jpg"],
-    # ["http://imgur.com/r/wallpaper/j39dKMi", "http://imgur.com/j39dKMi.jpg"],
-    # ["http://imgur.com/r/wallpaper/rZ37ZYN", "http://imgur.com/j39dKMi.jpg"],
-    # ["http://imgur.com/gallery/jm0OKQM", "http://imgur.com/j39dKMi.jpg"],
-    # ["http://imgur.com/gallery/oZXfZ", ""],
-  ].each do |url, first, last = nil, n = nil|
+    ["http://i.imgur.com/7xcxxkR.gifv", "http://i.imgur.com/7xcxxkRh.gif"], # gif
+    ["http://imgur.com/HQHBBBD", "https://i.imgur.com/HQHBBBD.jpg"], # http
+    ["https://imgur.com/BGDh6eu", "https://i.imgur.com/BGDh6eu.jpg"], # https
+    ["https://imgur.com/a/qNCOo", 6, "https://i.imgur.com/vwqfi3s.jpg", "https://i.imgur.com/CnSMWvo.jpg"], # https album
+    ["http://imgur.com/a/0MiUo", 49, "https://i.imgur.com/kJ1jrjO.jpg", "https://i.imgur.com/TMQJARX.jpg"], # album, zoomable
+    ["http://imgur.com/a/WswMg", 20, "https://i.imgur.com/Bt3RWV7.png", "https://i.imgur.com/sRc2lqN.png"], # album image, not zoomable
+    ["http://imgur.com/a/AdJUK", 3, "https://i.imgur.com/Yunpxnx.jpg", "https://i.imgur.com/2epn2nT.jpg"], # needs https because of authorship # WAT?
+    ["http://imgur.com/gallery/vR4Am", 7, "https://i.imgur.com/yuUQI25.jpg", "https://i.imgur.com/RdxyAMQ.jpg"],
+    ["http://imgur.com/gallery/qP2RQtL", "https://i.imgur.com/qP2RQtL.png"], # single image gallery?
+    ["http://imgur.com/gallery/jm0OKQM", "https://i.imgur.com/jm0OKQM.gif"],
+    ["http://imgur.com/gallery/nKAwE/new", 28, "https://i.imgur.com/VQhR8hB.jpg", "https://i.imgur.com/axlzNRL.jpg"],
+    ["http://m.imgur.com/rarOohr", "https://i.imgur.com/rarOohr.jpg"],
+    ["http://imgur.com/r/wallpaper/j39dKMi", "https://i.imgur.com/j39dKMi.jpg"],
+    ["http://imgur.com/gallery/oZXfZ", 12, "https://i.imgur.com/t7RjRXU.jpg", "https://i.imgur.com/anlPrvS.jpg"],
+  ].each do |url, n, first = nil, last = nil|
     real = Imgur::imgur_to_array url
-    unless n
-      fail [url, real].inspect unless real == Array(first)
+    unless last
+      fail [url, real.size, real].inspect unless real == Array(n)
     else
       fail [url, real.size].inspect unless real.size == n
       fail [url, real.first].inspect unless real.first == first
